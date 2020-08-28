1c6b01a2f554d6e2ec8b427e86826def0e55e360
Chintan Kukadiya <43231930+ckukadiya@users.noreply.github.com>
2020-08-28 17:13:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+const jsonServer = require('json-server')
+const server = jsonServer.create()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+server.post('/api/doppler', (req, res) => {
+  console.log('Doppler request received')
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router)
+server.listen(PORT, () => {
+  console.log('JSON Server is running')
+})
